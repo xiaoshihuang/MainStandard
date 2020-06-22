9468c3de2a76e4780f4f3846cce5a2ea5b04c7c1
xiaoshihuang <2499776990@qq.com>
2020-06-22 22:54:34 +0800

    备份到本地仓库

--- app/src/main/java/com/xintai/xhao/activity/MainFragementAct.java
+++ app/src/main/java/com/xintai/xhao/activity/MainFragementAct.java
@@ -45,7 +45,6 @@ public class MainFragementAct extends FragmentActivity implements View.OnClickLi
         setContentView(R.layout.act_main_fragment);
         initView();
         System.out.println("提交到远程gthub");
-        System.out.println("提交到远程gthub");
     }
 
     private void initView() {
